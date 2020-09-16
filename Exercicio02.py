3bb93fcfc52a868045fd80ac3d6fe1aa5f257f00
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-15 21:49:14 -0300

    feat: exercicio02

---
+++ Exercicio02.py
@@ -0,0 +1,14 @@
+ amigosProximos = {
+    "nome": "Cláudio",
+    "idade": 17   
+}
+amigosProximos['Thiago'] = 16
+
+amigosProximos['Vitor'] = 18
+
+amigosProximos['João Vitor'] = 18
+
+amigosProximos['Lucas'] = 18 
+
+print("Esses são meus amigos próximos: ", amigosProximos)
+
