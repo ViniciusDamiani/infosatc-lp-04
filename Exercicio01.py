3b246f434f51bda0bd0b90c0cea96f2dffc1f3c5
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-15 21:47:05 -0300

    feat: exercicio01

---
+++ Exercicio01.py
@@ -0,0 +1,5 @@
+jogadoresTupla = ("Ribamar","Neymar","German Cano","Castan","Ronaldinho")
+print("Esses são os top 5 melhores jogadores do mundo: ",jogadoresTupla)
+jogadoresTupla[4] = "Messi"
+print("Tá bom vai, vou trocar um jogador aqui", jogadoresTupla)
+#A Tupla é inalteravel, devemos tentar "Burlar" para fazer alguma alteração nela
