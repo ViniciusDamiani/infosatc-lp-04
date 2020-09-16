687d7b0a47cc2456ae59497ff823da4ef0348097
ViniciusDamiani <vinidamiani03@hotmail.com>
2020-09-15 21:50:31 -0300

    feat: exercicio03

---
+++ Exercicio03.py
@@ -0,0 +1,12 @@
+cadastro = {"id":1034, "Nome":"Vinicius", "CPF":30301515, "Telefone":96301654,"Email":"vinidamiani@"}
+
+cadastro['Endereço'] = 'Centro'
+cadastro['Sexo'] = 'Masculino'
+
+
+for i in cadastro.values():
+    print(i)
+
+#Essa letra c) eu entendi que era pra mostrar as duas formas
+print("Cadastro completo: ",cadastro)
+
